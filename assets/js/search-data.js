1436acd1d085f324cf952c996affd9850139c0db
danjruth <danjruth@gmail.com>
2026-03-29 15:32:43 +0000

    Deploying to gh-pages from @ danjruth/new-site-draft@dbd339062a3e2b10fc6e63f41bc1b31336431655 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/new-site-draft/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/new-site-draft/repositories/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",

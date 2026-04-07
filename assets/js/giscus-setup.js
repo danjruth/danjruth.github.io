6f5787dfe19b5d6555ac99f70a499151b989a687
danjruth <danjruth@gmail.com>
2026-04-07 11:46:28 +0000

    Deploying to gh-pages from @ danjruth/danjruth.github.io@44b9780b67fd251802c6ce519a29cb1a26c4f215 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "danjruth/new-site-draft",
+    "data-repo": "danjruth/danjruth.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -3,4 +3,4 @@ function gtag() {
   window.dataLayer.push(arguments);
 }
 gtag("js", new Date());
-gtag("config", "");
+gtag("config", "G-H71G6K48EG");
